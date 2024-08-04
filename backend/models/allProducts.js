a3762bc7a5fcfa20456dcd97b6a33a778d8fb8af
Sai sourav Sahu <saisouravsahu@gmail.com>
2024-08-04 22:16:33 +0530

    /allProducts api created

---
+++ backend/models/allProducts.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+
+
+const allProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  }
+});
+
+const Allproduct = new mongoose.model("Allproducts", userSchema);
+
+module.exports = Allproduct;
+//{name: 'Shoe', description: 'Good shoe very good', price: 100, stock: 2}
